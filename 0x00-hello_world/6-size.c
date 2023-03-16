f1de4ee673cb8303d01e4a6c9d40231fd318fe19
Gilfoye-Centrix <manidlamini@gmail.com>
2023-03-16 21:57:43 +0000

    hello world in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main() {
     printf("Size of char: %d byte(s)\n", sizeof(char));
     printf("Size of int: %d byte(s)\n", sizeof(int));
