37d1311ed16a3d6e5d0821bf96832e6f81d1eae6
Gilfoye-Centrix <manidlamini@gmail.com>
2023-04-13 18:50:43 +0000

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -128,3 +128,30 @@ while (i < len_r)
 _putchar(sum_result[i++] + '0');
 _putchar('\n');
 }
+
+int main(int argc, char **argv)
+{
+int len_1, len_2, len_r, temp, *sum_result;
+char *num1, *num2;
+
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+len_r = len_1 + len_2;
+
+if (len_1 < len_2)
+num1 = argv[1], num2 = argv[2];
+else
+{
+num1 = argv[2], num2 = argv[1];
+temp = len_2, len_2 = len_1, len_1 = temp;
+}
+sum_result = multiply(num1, len_1, num2, len_2, len_r);
+if (sum_result == NULL)
+exit(98);
+print_me(sum_result, len_r);
+return (0);
+}
