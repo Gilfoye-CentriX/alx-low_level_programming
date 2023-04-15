d0fae08f5bd96d414d736191febc0308d27ba94b
Gilfoye-Centrix <manidlamini@gmail.com>
2023-04-15 19:46:51 +0000

    argv argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Write a program that pritns its name,
+ * followed by a new line.
+ * @argc: Counts arguments
+ * @argv: Hold the argument that were input
+ * Return: return 0 if it was passed
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s", argv[i]);
+}
+
+printf("\n");
+return (0);
+}
