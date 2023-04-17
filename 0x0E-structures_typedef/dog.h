ca04f421d2686626a5099c018232c322082b3253
Gilfoye-Centrix <manidlamini@gmail.com>
2023-04-17 19:39:06 +0000

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,20 @@
-#ifndef _dog_h_
-#define _dog_h_
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - such structure, wow
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  */
-
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog_t;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
-#endif /* _dog_h_ */
-B
+#endif
