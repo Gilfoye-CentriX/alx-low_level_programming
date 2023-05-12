68a6e7c59035c1786a57140ad963f65b858a7880
Gilfoye-Centrix <manidlamini@gmail.com>
2023-05-12 21:35:34 +0000

    file IO

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,12 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void printclass(char *head);
+void printdata(char *head);
+void printversion(char *head);
+void printabi(char *head);
+void printtype(char *head);
+void printentry(char *head);
 
 int _putchar(char c);
 
